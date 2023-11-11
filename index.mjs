77dca581a170ccfe7e23f011a316a52a4ccb3676
Abhinav <abhinavmohanan17@gmail.com>
2023-11-11 11:30:04 +0530

    First commit

---
+++ index.mjs
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv'
+import express from 'express'
+
+dotenv.config();
+const app = express()
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    res.send("Kisan Mitra Backend")
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`)
+})
+
